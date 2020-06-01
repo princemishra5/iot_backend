0a3640c2293a501f0ecb28d77e412bd8706dd95f
princemishra5 <50906541+princemishra5@users.noreply.github.com>
2020-06-01 16:38:11 +0530

    Update HealthCheckController.java

--- src/main/java/com/example/demo/restservice/HealthCheckController.java
+++ src/main/java/com/example/demo/restservice/HealthCheckController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.atomic.AtomicLong;
 
